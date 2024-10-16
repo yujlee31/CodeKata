64d6a79bf61b68c579c3c0427aa7ea3e4b539314
yujlee31 <lyj.blessed@gmail.com>
2024-10-16 10:19:21 +0900

    -

---
+++ src/ck20241016.java
@@ -0,0 +1,16 @@
+public class ck20241016 {
+
+        public int solution(int num1, int num2) {
+            int answer = 0;
+
+            if (num1 >= -50000 && num1 <= 50000) {
+                if (num2 >= -50000 && num2 <= 50000) {
+                    answer = num1 - num2;
+                }
+            }
+
+
+            return answer;
+        }
+
+}
