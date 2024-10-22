9ea669be3ebd3c61911ed8f8ce5d978d14023cac
LYJ <lyj.blessed2@gmail.com>
2024-10-22 09:30:02 +0900

    init

---
+++ src/ck20241022.java
@@ -0,0 +1,9 @@
+public class ck20241022 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            double tmp = ((double)num1/(double)num2)*1000;
+            int answer = (int)tmp;
+            return answer;
+        }
+    }
+}
